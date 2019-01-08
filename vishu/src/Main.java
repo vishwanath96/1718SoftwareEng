1bdfbb3c40ce5f950f02d81d0f41a7751a9925a3
vishwanath96 <vishwanath.patil1996@gmail.com>
2019-01-08 12:32:02 +0530

    Elevator

---
+++ vishu/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Vishu ka Elevator!");
+        Elevator elevator =  new Elevator();
+        elevator.goTo(0);
+        elevator.goTo(4);
+        elevator.goTo(2);
+
+    }
+}
